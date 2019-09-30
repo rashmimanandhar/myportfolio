94619ba27ddc1347e1152baa3139f799f58711d7
rashmi <rashmi.manandhar7@gmail.com>
2019-09-30 15:16:03 -0400

    header html for portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="inner">
+        <nav>
+          <router-link to="/" id="logo">Rashmi Manandhar </router-link>
+          <ul>
+            <li>
+              <router-link to="/"> Home </router-link>
+              <router-link to="/about"> About </router-link>
+            </li>
+          </ul>
+        </nav>
+        <transition name="fade" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
