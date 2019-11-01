ae3d46a3ef29a9c0dd35ae661ac7bf71bc19167c
rashmi <rashmi.manandhar7@gmail.com>
2019-11-01 13:56:04 -0400

    create a component for portfolio

---
+++ src/components/PortfolioContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home main-content">
+    <div class="left-content slideUp">
+      <h1 :style="h1StyleObj">{{ content.title }}</h1>
+      <p class="subtxt" v-html="content.description"></p>
+      <router-link to="/portfolio/concordia">
+        <img src="../assets/arrow-down.svg" class="arrow arrow-down" alt="" />
+      </router-link>
+    </div>
+    <div class="right-content" :style="rightContentStyleObj">
+<!--      <img :src="require(`../assets/${content.image}`)" :style="imgStyleObj" />-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageColor: {
+      type: String,
+      required: true
+    },
+    imageColor:{
+      type: String,
+      required: true
+    },
+    content: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      h1StyleObj: {
+        color: this.pageColor + '!important'
+      },
+      rightContentStyleObj: {
+        background: this.imageColor,
+        backgroundImage: 'url('+require(`../assets/${this.content.image}`)+ ')',
+        // 'backgroundImage': require(`../assets/${this.content.image}`),
+        'background-size': 'contain',
+        'background-position': '50%',
+        'background-repeat': 'no-repeat'
+      },
+      imgStyleObj: {
+        width: '95%'
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/PortfolioFrontend.vue
@@ -0,0 +1,27 @@
+<template>
+  <PortfolioContent
+    :pageColor="'#7989AE'"
+    :imageColor="'#F0F3F7'"
+    :content="{
+      title: 'KUDO FRONTEND DEVELOPER',
+      description:
+        '<p>This system allows you to create multilingual conferences. You have the ability to join in the meeting and listen to the meeting in the language you desire. <br> <br>I was in charge of maintaining the system and any issues related to design. </p>',
+      image: 'session_console.png'
+    }"
+  />
+</template>
+
+<script>
+// @ is an alias to /src
+
+import PortfolioContent from '../components/PortfolioContent'
+
+export default {
+  name: 'KUDO',
+  components: {
+    PortfolioContent
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
